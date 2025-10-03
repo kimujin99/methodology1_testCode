99545dde977626296caac863b7cb9dffeee68aaf
loviewer <tomatoo3o97@gmail.com>
2025-10-03 20:48:07 +0900

    test_1. 상수로 구현하기

--- src/test/java/com/example/testCode/TestCodeApplicationTests.java
+++ src/test/java/com/example/testCode/TestCodeApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.testCode;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TestCodeApplicationTests {
-
 	@Test
-	void contextLoads() {
+	void testFibonacci() {
+		Assertions.assertEquals(0, fib(0));
+	}
+
+	int fib(int n) {
+		return 0;
 	}
 
 }
