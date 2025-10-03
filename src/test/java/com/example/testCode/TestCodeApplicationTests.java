a069baa14fe102dc49c170a9257e737df49eb466
loviewer <tomatoo3o97@gmail.com>
2025-10-03 20:53:09 +0900

    test_2. 두가지 케이스 만족하기

--- src/test/java/com/example/testCode/TestCodeApplicationTests.java
+++ src/test/java/com/example/testCode/TestCodeApplicationTests.java
@@ -9,10 +9,12 @@ class TestCodeApplicationTests {
 	@Test
 	void testFibonacci() {
 		Assertions.assertEquals(0, fib(0));
+		Assertions.assertEquals(1, fib(1));
 	}
 
 	int fib(int n) {
-		return 0;
+		if (n == 0) return 0;
+		return 1;
 	}
 
 }
